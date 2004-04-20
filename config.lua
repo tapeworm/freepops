3f37aa6da56bb58e5c691fbb2652203dd092609c
simone_vellei <>
2004-04-20 15:37:17 +0000

    fix

--- config.lua
+++ config.lua
@@ -45,6 +45,8 @@ freepops.MODULES_MAP["punto-informatico.it"] 	= {name="aggregator.lua",
 						args={host="http://punto-informatico.it/fader/pixml.txt"}}
 freepops.MODULES_MAP["linuxdevices.com"] 	= {name="aggregator.lua",
 						args={host="http://www.linuxdevices.com/backend/headlines.rdf"}}
+freepops.MODULES_MAP["securityfocus.com"] 	= {name="aggregator.lua",
+						args={host="http://www.securityfocus.com/rss/vulnerabilities.xml"}}
 
 -- -------------------------------------------------------------------------- --
 -- Customize here the paths for .lua and .so files

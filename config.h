e6fb25cd9c170248c47a7bcc944e2e0e7a19b156
ncocchiaro <>
2004-04-29 16:35:03 +0000

    stop bug

--- config.h
+++ config.h
@@ -16,7 +16,7 @@
 // common
 #define VERSION 	"0.0.1"
 #define PROGRAMNAME	"FreePOPs"
-#define PIDFILE 	"/var/run/freepops.pid"
+#define PIDFILE 	"/var/run/freepopsd.pid"
 #define POP3PORT  	2000
 #define MAXTHREADS 	5
 

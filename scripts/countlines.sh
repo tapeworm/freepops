111631df1f3a9ebc07d363666b932d2c6a0a63df
gareuselesinge <>
2004-06-21 09:10:28 +0000

    fix

--- scripts/countlines.sh
+++ scripts/countlines.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-CONTRIB="getdate getopt luasocket pthread regex tolua++ lua luadoc ."
+CONTRIB="getdate getopt pthread regex lua luadoc ."
 
 i=0
-for X in modules/src/*  ; do
+OLDDIR=`pwd`
+for X in modules/src/* src/ ; do
 	[ -d $X ]  || continue
 	[ `basename $X` != "CVS" ] || continue
 	modules[$i]=`basename $X`
 	cd $X
 	C[$i]=$(expr `find . -name \*.c -exec wc -l \{\} \; | cut -f 1 -d " " | tr -s "\n" "+" | sed s/\+/\ +\ /g` `find . -name \*.h -exec wc -l \{\} \; | cut -f 1 -d " " | tr -s "\n" "+" | sed s/\+/\ +\ /g` 0)
 	L[$i]=$(expr `find . -name \*.lua -exec wc -l \{\} \; | cut -f 1 -d " " | tr -s "\n" "+" | sed s/\+/\ +\ /g` `find . -name \*.pkg -exec wc -l \{\} \; | cut -f 1 -d " " | tr -s "\n" "+" | sed s/\+/\ +\ /g` 0)
-	cd ../../../
+	cd $OLDDIR
 	i=`expr $i + 1`
 done
 CC=0
@@ -25,15 +26,15 @@ for (( j = 0 ; j < $i ; j = j + 1 )); do
 done
 
 echo
-echo -e "\t+-----------------+-------+-------+"
-echo -e "\t|      module     |   C   |  LUA  |"
-echo -e "\t+-----------------+-------+-------+"
+echo -e "\t+----------------------+-------+-------+"
+echo -e "\t|         module       |   C   |  LUA  |"
+echo -e "\t+----------------------+-------+-------+"
 for (( j = 0 ; j < $i ; j = j + 1 )); do
-	printf "\t| %-15s | %5d | %5d |\n" ${modules[$j]} ${C[$j]} ${L[$j]}
+	printf "\t| %-20s | %5d | %5d |\n" ${modules[$j]} ${C[$j]} ${L[$j]}
 done
-echo -e "\t+-----------------+-------+-------+"
-printf "\t| %-15s | %5d | %5d |\n" "total*" $CC $LL
-echo -e "\t+-----------------+-------+-------+"
+echo -e "\t+----------------------+-------+-------+"
+printf "\t| %-20s | %5d | %5d |\n" "total*" $CC $LL
+echo -e "\t+----------------------+-------+-------+"
 echo
 echo -e "* without contrib modules:\n\t$CONTRIB"
 echo

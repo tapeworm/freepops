ae5155e2f5c6a376085ce25d2d0969a3d9816dac
gareuselesinge <>
2004-07-02 12:37:24 +0000

    fix manual

--- src/lua/foo.lua
+++ src/lua/foo.lua
@@ -46,7 +46,12 @@ function init(pstate)
 	if freepops.dofile("browser.lua") == nil then 
 		return POPSERVER_ERR_UNKNOWN 
 	end
-	
+
+	-- the common module
+	if freepops.dofile("common.lua") == nil then 
+		return POPSERVER_ERR_UNKNOWN 
+	end
+
 	-- checks on globals
 	freepops.set_sanity_checks()
 		
@@ -56,15 +61,15 @@ end
 -- Must save the mailbox name
 function user(pstate,username)
 	foo_globals.username = username
-	print("*** the user wants to login as '"..username.."'")
+	--print("*** the user wants to login as '"..username.."'")
 	return POPSERVER_ERR_OK
 end
 -- -------------------------------------------------------------------------- --
 -- Must login
 function pass(pstate,password)
 	foo_globals.password = password
-	print("*** the user inserted '"..password..
-		"' as the password for '"..foo_globals.username.."'")
+	--print("*** the user inserted '"..password..
+	--	"' as the password for '"..foo_globals.username.."'")
 	
 	-- create a new browser
 	local b = browser.new()
@@ -81,13 +86,14 @@ function pass(pstate,password)
 	local file,err = nil, nil
 	file,err = b:post_uri(post_uri,post_data)
 
-	print("we received this webpage: ".. file)
+	--print("we received this webpage: ".. file)
 
 	-- search the session ID
 	local _,_,id = string.find(file,"session_id=(%w+)")
 
 	if id == nil then 
 		return POPSERVER_ERR_AUTH
+	end
 
 	foo_globals.session_id = id
 
@@ -101,23 +107,76 @@ end
 -- -------------------------------------------------------------------------- --
 -- Update the mailbox status and quit
 function quit_update(pstate)
+	-- we need the stat
+	local st = stat(pstate)
+	if st ~= POPSERVER_ERR_OK then return st end
+
+	-- shorten names, not really important
+	local b = foo_globals.b
+	local post_uri = b:wherearewe() .. "/delete.php"
+	local session_id = foo_globals.session_id
+	local post_data = "session_id=" .. session_id .. "&"
+
+	-- here we need the stat, we build the uri and we check if we 
+	-- need to delete something
+	local delete_something = false;
+	
+	for i=1,get_popstate_nummesg(pstate) do
+		if get_mailmessage_flag(pstate,i,MAILMESSAGE_DELETE) then
+			post_data = post_data .. "check_" ..
+				get_mailmessage_uidl(pstate,i).. "=on&"
+			delete_something = true	
+		end
+	end
+
+	if delete_something then
+		b:post_uri(post_uri,post_data)
+	end
+
+	return POPSERVER_ERR_OK
 end
 -- -------------------------------------------------------------------------- --
 -- Fill the number of messages and their size
 function stat(pstate)
+	if foo_globals.stat_done == true then return POPSERVER_ERR_OK end
+	
 	local file,err = nil, nil
 	local b = foo_globals.browser
 	file,err = b:get_uri("http://localhost:3000/inbox.php?session_id="..
 		foo_globals.session_id)
 
-	local e = "*<tr>.*<td>.*<b>.*</b>.*</td>.*<td>.*<b>.*<a>"..
-		".*</a>.*</b>.*</td>.*<td>.*<b>.*</b>.*</td>.*<td>.*"..
-		"<b>.*</b>.*</td>.*<td>.*<input>.*</td>.*</tr>"
-	local g = "O<O>O<O>O<O>O<O>O<O>O<O>O<O>O<O>O<O>O<O>O<O>O"..
-		"<O>O<O>X<O>O<O>O<O>O<O>O<O>O<O>O<O>O<X>O<O>O<O>"
+	local e = ".*<tr>.*<td>[.*]{b}.*{/b}[.*]</td>.*<td>"..
+		"[.*]{b}.*<a>.*</a>.*{/b}[.*]</td>.*"..
+		"<td>[.*]{b}.*{/b}[.*]</td>.*<td>[.*]{b}.*{/b}[.*]</td>.*"..
+		"<td>.*<input>.*</td>.*</tr>"
+	local g = "O<O>O<O>[O]{O}O{O}[O]<O>O<O>[O]{O}O<O>O<O>O{O}[O]<O>O"..
+		"<O>[O]{O}X{O}[O]<O>O<O>[O]{O}O{O}[O]<O>O"..
+		"<O>O<X>O<O>O<O>"
 	local x = mlex.match(file,e,g)
+	
+	--debug print
+	--x:print()
 
+	set_popstate_nummesg(pstate,x:count())
 
+	for i=1,x:count() do
+		local _,_,size = string.find(x:get(0,i-1),"(%d+)")
+		local _,_,size_mult_k = string.find(x:get(0,i-1),"([Kk][Bb])")
+		local _,_,size_mult_m = string.find(x:get(0,i-1),"([Mm][Bb])")
+		local _,_,uidl = string.find(x:get(1,i-1),"check_(%d+)")
+
+		if size_mult_k ~= nil then
+			size = size * 1024
+		end
+		if size_mult_m ~= nil then
+			size = size * 1024 * 1024
+		end
+		set_mailmessage_size(pstate,i,size)
+		set_mailmessage_uidl(pstate,i,uidl)
+	end
+	
+	foo_globals.stat_done = true
+	return POPSERVER_ERR_OK
 end
 -- -------------------------------------------------------------------------- --
 -- Fill msg uidl field

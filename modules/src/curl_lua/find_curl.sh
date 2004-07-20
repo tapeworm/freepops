9396f2dc906e7fdbbc62f39c8ce8b1f1aefe393e
gareuselesinge <>
2004-07-20 12:55:49 +0000

    fixed Makefile for curl-config

---
+++ modules/src/curl_lua/find_curl.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ERRORMSG="unable_to_find_curl.h_you_should_edit_by_hand_the_curl_lua/Makefile"
+
+PREFIX=`curl-config --prefix`
+if [ -z "$PREFIX" ]; then
+	PATHS=`locate \*curl.h`
+	N=0
+	for X in $PATHS; do
+		N=`expr $N + 1`
+	done
+	if [ $N != 1 ]; then
+		echo $ERRORMSG
+	else
+		echo $PATHS
+	fi
+	
+else
+	HEADER="$PREFIX/include/curl/curl.h"
+	if [ -e $HEADER ]; then
+		echo $HEADER
+	else
+		HEADER="$PREFIX/include/curl.h"
+		if [ -e $HEADER ]; then
+			echo $HEADER
+		else
+			echo $ERRORMSG
+		fi
+	fi
+
+fi

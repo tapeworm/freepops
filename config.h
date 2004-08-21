2944b516e055eae2ddc0e60a00ba195a80786b1e
gareuselesinge <>
2004-08-21 12:05:35 +0000

    fix

--- config.h
+++ config.h
@@ -14,7 +14,7 @@
 /******************************************************************************/
 
 // common
-#define VERSION 	"0.0.14"
+#define VERSION 	"0.0.15"
 #define PROGRAMNAME	"FreePOPs"
 #define PIDFILE 	"/var/run/freepopsd.pid"
 #define POP3PORT  	2000

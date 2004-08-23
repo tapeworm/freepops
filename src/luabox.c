26823e2fbc8bd554d7d3e8a83ab0a7a71ff837b8
gareuselesinge <>
2004-08-23 18:35:37 +0000

    fix

---
+++ src/luabox.c
@@ -0,0 +1,75 @@
+/******************************************************************************
+ * $Id$
+ * This file is part of FreePOPs (http://freepops.sf.net)                     *
+ * This file is distributed under the terms of GNU GPL license.               *
+ ******************************************************************************/
+
+/******************************************************************************
+ * File description:
+ *	lua VM generation stuff
+ * Notes:
+ *
+ * Authors:
+ * 	Enrico Tassi <gareuselesinge@users.sourceforge.net>
+ ******************************************************************************/
+
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+#include "luay.h"
+
+#include "luabox.h"
+
+#include "pop3server_lua.h"
+#include "log_lua.h"
+#include "mlex_lua.h"
+#include "stringhack_lua.h"
+#include "session_lua.h"
+#include "curl_lua.h"
+#include "getdate_lua.h"
+#include "psock_lua.h"
+#include "base64_lua.h"
+#include "regularexp_lua.h"
+#include "lxplib.h"
+
+#include "log.h"
+#define LOG_ZONE "LUABOX"
+
+static int (*opening_functions[LUABOX_LAST])(lua_State*) = {
+	luaopen_base,
+	luaopen_table,
+	luaopen_io,
+	luaopen_string,
+	luaopen_math,
+	luaopen_debug,
+	luaopen_loadlib,
+	luaopen_pop3server,
+	luaopen_mlex,
+	luaopen_stringhack,
+	luaopen_session,
+	luacurl_open,
+	luaopen_psock,
+	luaopen_base64,
+	luaopen_getdate,
+	luaopen_regularexp,
+	luaopen_lxp,
+	luaopen_log
+};
+
+lua_State* luabox_genbox(unsigned long intial_stuff){
+	lua_State* tmp = lua_open();
+	MALLOC_CHECK(tmp);
+	luabox_addtobox(tmp,intial_stuff);
+	return tmp;
+}
+
+void luabox_addtobox(lua_State* box,unsigned long stuff){
+	long int i;
+	for ( i = 0 ; i < LUABOX_LAST ; i++) {
+		int j = 1<<i;
+		if (j & stuff) 
+			opening_functions[i](box);
+	}
+	luay_emptystack(box);
+}
+
---
+++ src/luabox.h
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * $Id$
+ * This file is part of FreePOPs (http://freepops.sf.net)                     *
+ * This file is distributed under the terms of GNU GPL license.               *
+ ******************************************************************************/
+
+
+/******************************************************************************/
+ /*!
+  * \file   luabox.h
+  * \brief  luaVM generation module
+  * \author Enrico Tassi gareuselesinge<@users.sourceforge.net>
+  */
+/******************************************************************************/
+
+#ifndef LUABOX_H
+#define LUABOX_H
+
+#define LUABOX_EMPTY	0
+
+#define LUABOX_BASE	(1<<0)
+#define LUABOX_TABLE	(1<<1)
+#define LUABOX_IO	(1<<2)
+#define LUABOX_STRING	(1<<3)
+#define LUABOX_MATH	(1<<4)
+#define LUABOX_DEBUG	(1<<5)
+#define LUABOX_LOADLIB	(1<<6)
+
+#define LUABOX_STANDARD	(LUABOX_BASE|LUABOX_TABLE|LUABOX_IO|\
+			 LUABOX_STRING|LUABOX_MATH|LUABOX_DEBUG|\
+			 LUABOX_LOADLIB)
+
+#define LUABOX_POP3SERVER	(1<<7)
+#define LUABOX_MLEX		(1<<8)
+#define LUABOX_STRINGHACK	(1<<9)
+#define LUABOX_SESSION		(1<<10)
+#define LUABOX_CURL		(1<<11)
+#define LUABOX_PSOCK		(1<<12)
+#define LUABOX_BASE64		(1<<13)
+#define LUABOX_GETDATE		(1<<14)
+#define LUABOX_REGULAREXP	(1<<15)
+#define LUABOX_LXP		(1<<16)
+#define LUABOX_LOG		(1<<17)
+
+#define LUABOX_FREEPOPS	(LUABOX_POP3SERVER|LUABOX_MLEX|LUABOX_STRINGHACK|\
+			 LUABOX_SESSION|LUABOX_CURL|LUABOX_PSOCK|\
+			 LUABOX_BASE64|LUABOX_GETDATE|LUABOX_REGULAREXP|\
+			 LUABOX_LXP|LUABOX_LOG)
+
+#define LUABOX_FULL (LUABOX_STANDARD|LUABOX_FREEPOPS)
+
+#define LUABOX_LAST 18
+
+//! generates a luaVM loading initial_stuff libraries
+lua_State* luabox_genbox(unsigned long intial_stuff);
+
+//! loads into box the stuff libraries
+void luabox_addtobox(lua_State* box,unsigned long stuff);
+
+#endif 

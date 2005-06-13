c5c322a82c9d64b4a27682482267ce3bc72c0b22
gareuselesinge <>
2005-06-13 16:55:52 +0000

    more work for updater

--- modules/src/updater_lua/updater.lua
+++ modules/src/updater_lua/updater.lua
@@ -2,6 +2,7 @@ assert(freepops.loadlib("browser.lua"))()
 assert(freepops.loadlib("xml2table.lua"))()
 assert(freepops.loadlib("table2xml.lua"))()
 assert(freepops.loadlib("plugins2xml.lua"))() 
+assert(freepops.loadlib("version_comparer.lua"))() 
 
 b = nil
 FP = "http://www.freepops.org/"
@@ -33,7 +34,10 @@ function get_online_versions(plugins)
 		local txml = xml2table.xml2table(xml)
 		local version = txml.version._content
 		local require_version = txml.require_version._content
-		v[name] = {version = version, require_version = require_version}
+		local link = txml.url._content
+		v[name] = {version = version, 
+			require_version = require_version,
+			link = link}
 	end)
 	return v
 end
@@ -45,7 +49,9 @@ function get_local_versions(plugins)
 		local txml = plugins2xml.extract(file)
 		local version = txml.version._content
 		local require_version = txml.require_version._content
-		v[name] = {version = version, require_version = require_version}
+		v[name] = {version = version, 
+			require_version = require_version,
+			path = file}
 	end)
 	return v
 end
@@ -59,21 +65,81 @@ function main(plugins)
 	end
 	local versions = get_online_versions(plugins)
 	local old_versions = get_local_versions(plugins)
+	local todo = {}
 	
+	-- load the todo
 	table.foreach(old_versions, function(name,v)
 		local avail = versions[name] or {}
 		local version = avail.version or ""
+		local vc = version_comparer 
 		log.dbg("For " .. name .. " " .. v.version .. " >= " .. version)
-		if not freepops.is_version_ge(v.version, version) then
+		local rc = vc.compare_versions(v.version,version)
+		if rc == nil then
+			log.error_print("Versions uncomparable: " .. 
+				v.version .. " " ..  version)
+			log.error_print("We try the upgrade!")
+			rc = -1
+		end
+		if rc == -1 then
 			-- new version available
 			local new = freepops.enough_new(avail.require_version)
 			if not new then
-				print(name, "is upgradable (new core needed)")
+				log.say("A new version of " .. name .. 
+				"is available, but will not be downladed "..
+				"since it needs a newer core, "..
+				"at least version "..avail.require_version)
 			else
-				print(name, "is upgradable")
+				log.say(name .. " will be upgraded")
+				table.insert(todo, {
+					name = name, 
+					link = avail.link, 
+					path = v.path})
 			end
 		end
+	end)
+
+	-- some helpers
+	local useragent = {
+		"useragent: curl/"..curl.version().. " () FreePOPs/" ..
+		 os.getenv("FREEPOPS_VERSION")
+	}
+	local function check_fd(f, err, what, path)
+		if f == nil then
+			log.error_print("Unable to " .. what .. " " .. path)
+			log.error_print("error: " .. err)
+			return true
+		end
+		return false
+	end
+	
+	-- update
+	table.foreach(todo, function(_, v) 
+		local f, data = nil, nil
+		local backup = v.path .. ".bak"
+		local online_data, err = nil, nil
+		log.dbg("  Begin upgrade of " .. v.name)
+		log.dbg("  Downloading " .. v.link)
+		online_data, err = b:get_uri(v.link, useragent)
+		if check_fd(online_data, err, "download", v.link)then return end
+		log.dbg("  Backup " .. v.path .. " in " .. backup)
+		f, err = io.open(v.path,"r")
+		if check_fd(f, err, "read", v.path) then return end
+		data = f:read("*a")
+		f:close()
+		f = io.open(backup ,"w")
+		if check_fd(f, err, "write", backup) then return end
+		f:write(data)
+		f:close()
+		log.dbg("  Writing new " .. v.path)
+		f = io.open(v.path,"w")
+		if check_fd(f, err, "write", v.path) then return end
+		f:write(online_data)
+		f:close()
+		log.dbg("End upgrade of " .. v.name)
+		log.say(name .. " updated")
 	end)
 	
 	return 0
 end
+
+-- eof
---
+++ modules/src/version_comparer_lua/version_comparer.lua
@@ -0,0 +1,100 @@
+---
+-- version_comparer functions.
+-- This module implements a smart version somparer.
+
+version_comparer = {}
+
+--============================================================================---- This is part of FreePOPs (http://www.freepops.org) released under GNU/GPL
+--============================================================================--
+
+
+local entries = {
+	{rex="^([%a]+)",type="token"},
+	{rex="^([%d]+)",type="token"},
+	{rex="^(%.)",type="spacer"},
+	{rex="^(%-)",type="spacer"},
+	{rex="^(%:)",type="spacer"},
+	{rex="^(%+)",type="spacer"}
+}
+
+local private = {}
+
+function private.next_token(s)
+	local s1, tok = nil, nil
+	if s == nil then return nil, nil end
+	table.foreachi(entries, function(_, e)
+		local from, _, capture = string.find(s, e.rex)
+		if capture ~= nil then
+			s1 = string.sub(s,from + string.len(capture),-1)
+			tok = {val=capture, type=e.type}
+			return true -- stop looping
+		end
+	end)
+	return s1, tok
+end
+
+function private.compare_token(tok1, tok2)
+	if tok1 == nil and tok2 == nil then return 0 end
+	if tok1 == nil and tok2 ~= nil then return -1 end
+	if tok1 ~= nil and tok2 == nil then return 1 end
+	if tok1.type == "spacer" and tok1.type == "spacer" then 
+		if tok1.val == tok2.val then return 0 end
+		return nil
+	end
+	if tok1.type == "spacer" and tok1.type ~= "spacer" then return 1 end
+	if tok1.type ~= "spacer" and tok1.type == "spacer" then return -1 end
+	local ty1 = tonumber(tok1.val)
+	local ty2 = tonumber(tok2.val)
+	if ty1 == nil or ty2 == nil then
+		if tok1.val == tok2.val then return 0 end
+		if tok1.val <  tok2.val then return -1 end
+		if tok1.val >  tok2.val then return 1 end
+	else
+		if ty1 - ty2 == 0 then return 0 end
+		if ty1 - ty2 <  0 then return -1 end
+		if ty1 - ty2 >  0 then return 1 end
+	end
+end
+
+---
+-- Compare version1 and version2.
+-- @param version1 string 
+-- @param version1 string 
+-- @return number 0 if version1 == version2, 
+-- 		-1 if version1 < version2, 
+-- 		1 if version1 > version2, nil if incomparable.
+function version_comparer.compare_versions(version1, version2)
+	local v1, tok1 = private.next_token(version1)	
+	local v2, tok2 = private.next_token(version2)	
+
+	local rc = private.compare_token(tok1, tok2)
+	if rc == 0 and (v1 ~= nil or v2 ~= nil) then
+		return version_comparer.compare_versions(v1, v2)
+	else
+		return rc
+	end
+end
+
+--function test()
+--	local todo = {
+--		{v1="1",		v2="1",			rc=0},
+--		{v1="2",		v2="1",			rc=1},
+--		{v1="1",		v2="2",			rc=-1},
+--		{v1="1.0.1",		v2="1.0.1b",		rc=-1},
+--		{v1="1.0.2",		v2="1.0.1",		rc=1},
+--		{v1="1.1.0",		v2="1.0.99",		rc=1},
+--		{v1="1.99.0",		v2="1.9.1",		rc=1},
+--		{v1="1.0+p2",		v2="1.0+p1",		rc=1},
+--		{v1="1.0a-1",		v2="1.0-2",		rc=1},
+--		{v1="1.9a-2",		v2="1.99-1",		rc=-1},
+--	}
+--	table.foreach(todo, function(_, v)
+--		local rc = compare_versions(v.v1, v.v2)
+--		if rc ~= v.rc then
+--			print("FAIL", v.v1, "Vs", v.v2)
+--		else
+--			print("OK", v.v1, "Vs", v.v2)
+--		end
+--	end)
+--end
+--test()

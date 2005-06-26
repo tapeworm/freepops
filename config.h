a0a4f17515c9821cfda6e6e65e9b94a66f96e4b9
gareuselesinge <>
2005-06-26 13:40:50 +0000

    minor fixes

--- config.h
+++ config.h
@@ -14,7 +14,7 @@
 /******************************************************************************/
 
 // common
-#define VERSION 	"0.0.29"
+#define VERSION 	"0.0.30"
 #define PROGRAMNAME	"FreePOPs"
 #define PIDFILE 	"/var/run/freepopsd.pid"
 #define POP3PORT  	2000

9bb9348dcffeed96cc07d6739f3ba805383d979a
russell822 <>
2005-07-15 14:12:49 +0000

    fixed some yahoo domains

--- config.lua
+++ config.lua
@@ -58,9 +58,10 @@ freepops.MODULES_MAP["yahoo.ca"] 	= {name="yahoo.lua"}
 freepops.MODULES_MAP["yahoo.co.in"] 	= {name="yahoo.lua"}
 freepops.MODULES_MAP["yahoo.com.tw"] 	= {name="yahoo.lua"}
 freepops.MODULES_MAP["yahoo.co.uk"] 	= {name="yahoo.lua"}
-freepops.MODULES_MAP["yahoo.co.cn"] 	= {name="yahoo.lua"}
+freepops.MODULES_MAP["yahoo.com.cn"] 	= {name="yahoo.lua"}
 freepops.MODULES_MAP["yahoo.es"] 	= {name="yahoo.lua"}
-freepops.MODULES_MAP["yahoo.dk"]        = {name="yahoo.lua"} 
+freepops.MODULES_MAP["yahoo.de"]        = {name="yahoo.lua"} 
+freepops.MODULES_MAP["rocketmail.com"]  = {name="yahoo.lua"} 
 
 -- hotmail
 freepops.MODULES_MAP["hotmail.com"]		= {name="hotmail.lua"}

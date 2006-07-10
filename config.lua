ea36f2e2ed7b9d616acd90f561ec88f263fd991e
russell822 <>
2006-07-10 13:08:15 +0000

    Added india.com

--- config.lua
+++ config.lua
@@ -426,6 +426,7 @@ freepops.MODULES_MAP["mail.org" ]		= {name="mailcom.lua"}
 freepops.MODULES_MAP["italymail.com" ]		= {name="mailcom.lua"}
 freepops.MODULES_MAP["computer4u.com"]		= {name="mailcom.lua"}
 freepops.MODULES_MAP["mexico.com"]		= {name="mailcom.lua"}
+freepops.MODULES_MAP["india.com"]		= {name="mailcom.lua"}
 
 -- mail2world
 freepops.MODULES_MAP["mail2.*%.com" ]		= {name="mail2world.lua", 

2b43dfa56c312e0793dc30ee27fc99bfe8751d3d
gareuselesinge <>
2007-01-14 15:14:20 +0000

    added updater link under win32, moved to config.h some defines that were direclty passed to gcc

--- config.h
+++ config.h
@@ -27,7 +27,8 @@
 	#define DEFAULT_USERAGENT \
 		"Mozilla/5.0 (; U; Win32; en-US; rv:1.7.8)"\
 		" Gecko/20050518 Firefox/1.0.4"
-	//#define FREEPOPSLUA_USER_UNOFFICIAL "lua_unofficial"
+	#define FREEPOPSLUA_PATH_UPDATES "lua_updates/"
+	#define FREEPOPSLUA_PATH_UNOFFICIAL "lua_unofficial/"
 #endif
 
 // beos
@@ -37,7 +38,8 @@
 	#define DEFAULT_USERAGENT \
 		"Mozilla/5.0 (X11; U; BeOS; en-US; rv:1.7.8)"\
 		" Gecko/20050518 Firefox/1.0.4"
-	//#define FREEPOPSLUA_USER_UNOFFICIAL "lua_unofficial"
+	#define FREEPOPSLUA_PATH_UPDATES "/var/lib/freepops/lua_updates/"
+	#define FREEPOPSLUA_PATH_UNOFFICIAL "/var/lib/freepops/lua_unofficial/"
 #endif
 
 #ifdef MACOSX
@@ -46,7 +48,8 @@
 	#define DEFAULT_USERAGENT \
 		"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; "\
 		"en-US; rv:1.7.5) Gecko/20041107 Firefox/1.0"
-	//#define FREEPOPSLUA_USER_UNOFFICIAL "lua_unofficial"
+	#define FREEPOPSLUA_PATH_UPDATES "src/lua_updates/"
+	#define FREEPOPSLUA_PATH_UNOFFICIAL "src/lua_unofficial/"
 #endif
 		
 // unix
@@ -56,8 +59,8 @@
 	#define DEFAULT_USERAGENT \
 		"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.7.8)"\
 		" Gecko/20050518 Firefox/1.0.4"
-	//#define FREEPOPSLUA_USER_UNOFFICIAL "%s/.freepops/lua_unofficial"
-	
+	#define FREEPOPSLUA_PATH_UPDATES "/var/lib/freepops/lua_updates/"
+	#define FREEPOPSLUA_PATH_UNOFFICIAL "/var/lib/freepops/lua_unofficial/"
 #endif
 
 

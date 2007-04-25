28a2e61c71c5741257f253ca820e6e6df6116355
gareuselesinge <>
2007-04-25 16:00:05 +0000

    bumped modules version

--- modules/src/updater_lua/updater_common.lua
+++ modules/src/updater_lua/updater_common.lua
@@ -4,7 +4,7 @@
 
 MODULE_VERSION = "0.0.1"
 MODULE_NAME = "updater_common"
-MODULE_REQUIRE_VERSION = "0.2.0"
+MODULE_REQUIRE_VERSION = "0.2.3"
 MODULE_LICENSE = "GNU/GPL"
 MODULE_URL = "http://www.freepops.org/download.php?module=updater_common.lua"
 MODULE_HOMEPAGE = "http://www.freepops.org/"

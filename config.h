722f0da937c36ddfa546d657d483b9246bbd5c24
gareuselesinge <>
2008-05-24 16:19:25 +0000

    0.2.7

--- config.h
+++ config.h
@@ -14,7 +14,7 @@
 /******************************************************************************/
 
 // common
-#define VERSION 	"0.2.6"
+#define VERSION 	"0.2.7"
 #define PROGRAMNAME	"FreePOPs"
 #define PIDFILE 	"/var/run/freepopsd.pid"
 #define POP3PORT  	2000

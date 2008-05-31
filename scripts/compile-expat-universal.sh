225c1331fe64619933d785910d3107f724e01f40
gareuselesinge <>
2008-05-31 13:25:43 +0000

    simpler scripts to build expat and ftk for osx

--- scripts/compile-expat-universal.sh
+++ scripts/compile-expat-universal.sh
@@ -6,23 +6,8 @@ set -e
 rm -rf expat-1.95.8
 tar -xvzf expat-1.95.8.tar.gz
 cd expat-1.95.8
-CFLAGS="-arch ppc -isysroot /Developer/SDKs/MacOSX10.3.9.sdk/ -mmacosx-version-min=10.3" \
-LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.3.9.sdk/" \
-./configure --prefix=/Users/enricotassi/freepops/lib --disable-shared
-CFLAGS="-arch ppc -isysroot /Developer/SDKs/MacOSX10.3.9.sdk/ -mmacosx-version-min=10.3" \
-LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.3.9.sdk/" \
-make || (/bin/sh ./libtool --silent --mode=link gcc -Wl,-arch,ppc -isysroot /Developer/SDKs/MacOSX10.3.9.sdk/ -mmacosx-version-min=10.3 -Wall -Wmissing-prototypes -Wstrict-prototypes -fexceptions -DHAVE_EXPAT_CONFIG_H   -I./lib -I. -no-undefined -version-info 6:0:5 -rpath /Users/enricotassi/freepops/lib-ppc-10.3/lib -Wl:-Wl,-syslibroot,/Developer/SDKs/MacOSX10.3.9.sdk/ -o libexpat.la lib/xmlparse.lo lib/xmltok.lo lib/xmlrole.lo && make) 
-mv .libs .libs.10.3
-make clean
-CFLAGS="-arch i386 -arch x86_64 -arch ppc64 -isysroot /Developer/SDKs/MacOSX10.4u.sdk/ -mmacosx-version-min=10.4" \
+CFLAGS="-arch ppc -arch i386 -arch x86_64 -arch ppc64 -isysroot /Developer/SDKs/MacOSX10.4u.sdk/ -mmacosx-version-min=10.4" \
 LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk/" \
-./configure --prefix=/Users/enricotassi/freepops/lib --disable-shared
-CFLAGS="-arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk/ -mmacosx-version-min=10.4" \
-LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk/" \
-make || (/bin/sh ./libtool --silent --mode=link gcc -Wl,-arch,i386 -Wl,-arch,x86_64 -Wl,-arch,ppc64 -isysroot /Developer/SDKs/MacOSX10.4u.sdk/ -mmacosx-version-min=10.4 -Wall -Wmissing-prototypes -Wstrict-prototypes -fexceptions -DHAVE_EXPAT_CONFIG_H   -I./lib -I. -no-undefined -version-info 5:0:5 -rpath /Users/enricotassi/freepops/lib/lib -Wl,-Wl:-syslibroot:/Developer/SDKs/MacOSX10.4u.sdk/ -o libexpat.la lib/xmlparse.lo lib/xmltok.lo lib/xmlrole.lo && make)
-mv .libs .libs.10.4
-mkdir .libs/
-lipo -create -output .libs/libexpat.a .libs.10.3/libexpat.a .libs.10.4/libexpat.a
-cp -RP .libs.10.3/*la .libs.10.3/*lai .libs
-sudo make install
+./configure --prefix=/Users/gares/freepops/ --disable-shared && \
+make && sudo make install
 file .libs/libexpat.a
--- scripts/compile-fltk-universal.sh
+++ scripts/compile-fltk-universal.sh
@@ -3,29 +3,24 @@
 set -e
 set -x
 
+PREFIX=/Users/gares/freepops/
+
 rm -rf fltk-1.1.7
 tar -xvzf fltk-1.1.7-source.tar.gz
 cd fltk-1.1.7
-CFLAGS="-arch ppc -isysroot /Developer/SDKs/MacOSX10.3.9.sdk/ -mmacosx-version-min=10.3" \
-CXXFLAGS="-arch ppc -isysroot /Developer/SDKs/MacOSX10.3.9.sdk/ -mmacosx-version-min=10.3" \
-LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.3.9.sdk/" \
-./configure --prefix=/Users/enricotassi/freepops/lib --disable-gl --disable-threads 
-make
-for X in lib/*.a; do mv $X $X.10.3; done
-mv fluid/fluid fluid/fluid.10.3
-make clean
-CFLAGS="-arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk/ -mmacosx-version-min=10.4" \
-CXXFLAGS="-arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk/ -mmacosx-version-min=10.4" \
-LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk/" \
-./configure --prefix=/Users/enricotassi/freepops/lib --disable-gl --disable-threads
-make
-for X in lib/*.a; do mv $X $X.10.4; done
-mv fluid/fluid fluid/fluid.10.4
-lipo -create -output lib/libfltk.a lib/libfltk.a.10.3 lib/libfltk.a.10.4
-lipo -create -output lib/libfltk_forms.a lib/libfltk_forms.a.10.3 lib/libfltk_forms.a.10.4
-lipo -create -output lib/libfltk_images.a lib/libfltk_images.a.10.3 lib/libfltk_images.a.10.4
-lipo -create -output lib/libfltk_jpeg.a lib/libfltk_jpeg.a.10.3 lib/libfltk_jpeg.a.10.4
-lipo -create -output lib/libfltk_png.a lib/libfltk_png.a.10.3 lib/libfltk_png.a.10.4
-lipo -create -output fluid/fluid fluid/fluid.10.3 fluid/fluid.10.4
+mkdir private
+for ARCH in ppc i386; do
+	make clean
+	CFLAGS="-arch $ARCH -isysroot /Developer/SDKs/MacOSX10.4u.sdk/ -mmacosx-version-min=10.4" \
+	CXXFLAGS="-arch $ARCH -isysroot /Developer/SDKs/MacOSX10.4u.sdk/ -mmacosx-version-min=10.4" \
+	LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk/" \
+	./configure --prefix=$PREFIX --disable-gl --disable-threads && make
+	for X in lib/*.a; do mv $X private/`basename $X`.$ARCH; done
+	mv fluid/fluid private/fluid.$ARCH
+done
+
+for X in lib/libfltk.a lib/libfltk_forms.a lib/libfltk_images.a lib/libfltk_jpeg.a lib/libfltk_png.a fluid/fluid; do
+	lipo -create -output $X private/`basename $X`.ppc private/`basename $X`.i386
+done
 file lib/libfltk.a
 sudo make install

2f92d4bf37dbec62470faafd338bb06ad2fadae2
gareuselesinge <>
2011-06-24 14:00:23 +0000

    refreshed version

--- src/lua/libero.lua
+++ src/lua/libero.lua
@@ -27,7 +27,7 @@
 -- fill them in the right way
 
 -- single string, all required
-PLUGIN_VERSION = "0.2.23"
+PLUGIN_VERSION = "0.2.24"
 PLUGIN_NAME = "Libero.IT"
 PLUGIN_REQUIRE_VERSION = "0.2.0"
 PLUGIN_LICENSE = "GNU/GPL"
